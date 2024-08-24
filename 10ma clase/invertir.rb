3aae943c4ce617cb49db5458144daf76d5173857
IsmaelRuby <ismael@palmatellez.cl>
2024-08-24 00:25:27 -0400

    24th august

---
+++ 10ma clase/invertir.rb
@@ -0,0 +1,9 @@
+colors = 
+{
+'red' => '#cc0000', 
+'green' =>'#00cc000', 
+'blue' => '#0000cc'
+}
+
+puts (colors.invert['#cc0000'])
+#red
---
+++ 10ma clase/obtener_valores.rb
@@ -0,0 +1,8 @@
+ventas = {
+Enero: 65000,
+Febrero: 68000,
+Marzo: 72000
+}
+
+puts ventas.keys
+puts ventas.values
